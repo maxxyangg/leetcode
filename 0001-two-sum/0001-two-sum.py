40c88847252d9435ec3ec88c9ccd4539c9a64d51
Maximillian Yang <61856860+maxxyangg@users.noreply.github.com>
2023-09-21 22:47:49 -0500

    Time: 57 ms (92.72%), Space: 17.5 MB (56.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_indices = {}  # Dictionary to store numbers and their indices
         
-        result = []
-
-        for x in range(len(nums)):
-            first_number = nums[x]
-            for y in range(x+1, len(nums)):
-                second_number = nums[y]
-                if(first_number + second_number == target):
-                    result.append(x)
-                    result.append(y)
-                    return result
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in num_indices:
+                return [num_indices[complement], index]
+            num_indices[num] = index
+        
+        return []
